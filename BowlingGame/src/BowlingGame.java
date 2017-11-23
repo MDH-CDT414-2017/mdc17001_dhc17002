8bbfd79bef3faf54a01caa3fd2ed68b6d05517b5
Marija Đorđević <masadordevic@gmail.com>
2017-11-23 14:59:33 +0100

    Given test cases

--- BowlingGame/src/BowlingGame.java
+++ BowlingGame/src/BowlingGame.java
@@ -1,27 +1,121 @@
-/** BowlingGame Score calculator 
+import java.util.ArrayList;
+
+/**
+ * BowlingGame Score calculator
  *
- * @author CDT414 Student: 
- * @version 1.0 
+ * @author CDT414 Student:
+ * @version 1.0
  * @date 2016-11-24
  */
 public class BowlingGame {
 
-	/** BowlingGame Score calculator constructor which require string as input 
-	 * @param game Expected format "[n,n][n,n]..[n,n]"
-	 * 
-	 */	 
-	public BowlingGame(String game)
-	{	
-		//TODO: create constructor for BowlingGame
+	ArrayList<Integer> frames;
+
+	public BowlingGame(String game) {
+		this.frames = this.parse(game);
+
 	}
-	
-	/** getScore method returns a score of current Bowling game or -1 if error
-	 * 
-	 * @return Integer value of Bowling score, in case of error return value is -1 
-	 */
+
+	public boolean isStrike(Integer a, Integer b) {
+		return this.getType(a, b) == "strike";
+	}
+
+	public boolean isSpare(Integer a, Integer b) {
+		return this.getType(a, b) == "spare";
+	}
+
+	public boolean isOpen(Integer a, Integer b) {
+		return this.getType(a, b) == "open";
+	}
+
+	public String getType(Integer a, Integer b) {
+		if (a == 10) {
+			return "strike";
+		}
+		if (a + b == 10) {
+			return "spare";
+		}
+		if (a + b < 10 && a + b >= 0) {
+			return "open";
+		}
+		return "error";
+	}
+
+	public ArrayList<Integer> parse(String game) {
+		String[] strFrames = game.trim().split("]");
+		ArrayList<Integer> frames = new ArrayList<Integer>();
+		for (String frame : strFrames) {
+			frame = frame.substring(1);
+			if (frame.contains(",")) {
+				for (String framePart : frame.split(",")) {
+					frames.add(Integer.parseInt(framePart));
+				}
+			} 
+			else {
+				frames.add(Integer.parseInt(frame));
+				frames.add(Integer.parseInt("0"));
+			}
+
+		}
+		return frames;
+	}
+
+	public int getNext(int num, int pos) {
+		int ret = 0;
+		for (int i = pos; i < pos + num && i < this.frames.size(); i++) {
+			if(this.frames.get(i) == 0 && this.frames.get(i-1) == 10) {
+				num++;
+				continue;				
+			}
+			ret += this.frames.get(i);
+		}
+		return ret;
+	}
+
 	public int getScore() {
-		//TODO: calculate the score of game and return correct value
-		return(-1);
+		int score = 0;
+		if (!this.isFrameCountCorrect()) {
+			return -1;
+		}
+		for (int i = 0; i < 20; i += 2) {
+			int a = this.frames.get(i);
+			int b = this.frames.get(i + 1);
+			int lastStrike = score;
+			String type = this.getType(a, b);
+			if (type == "error") {
+				return -1;
+			}
+
+			if (type == "open") {
+				score += a + b;
+			}
+
+			if (type == "strike") {
+				score += a + b + this.getNext(2, i + 2);
+			}
+
+			if (type == "spare") {
+				score += a + b + this.getNext(1, i + 2);
+			}
+			int djasnjdajd = 5;
+			djasnjdajd = 10;
+		}
+
+		return score;
 	}
-	
+
+	public boolean isFrameCountCorrect() {
+		int frameCount = frames.size() / 2;
+
+		if (frameCount < 10) {
+			return false;
+		}
+		String lastFrameType = this.getType(18, 19);
+		if ((lastFrameType == "open" && frameCount != 20) || (lastFrameType == "spare" && frameCount != 22)
+				|| (lastFrameType == "strike" && frameCount != 23)) {
+			return false;
+		}
+		return true;
+	}
+
 }
--- BowlingGame/src/BowlingGameTest.java
+++ BowlingGame/src/BowlingGameTest.java
@@ -7,9 +7,62 @@ import org.junit.jupiter.api.Test;
 class BowlingGameTest {
 
 	@Test
-	public void test01() {
-        BowlingGame bowlingGame = new BowlingGame("");
-        assertEquals(-1, bowlingGame.getScore());
-    }	
+	public void open1() {
+		BowlingGame b = new BowlingGame("[1,5][3,6][7,2][3,6][4,4][5,3][3,3][4,5][8,1][2,6]");
+		assertEquals(81, b.getScore());
+	}
+	
+	@Test
+	public void strike1() {
+		BowlingGame b = new BowlingGame("[10,0][3,6][7,2][3,6][4,4][5,3][3,3][4,5][8,1][2,6]");
+		assertEquals(94, b.getScore());
+	}
+	
+	@Test
+	public void spare1() {
+		BowlingGame b = new BowlingGame("[1,9][3,6][7,2][3,6][4,4][5,3][3,3][4,5][8,1][2,6]");
+		assertEquals(88, b.getScore());
+	}
+	
+	@Test
+	public void spare2() {
+		BowlingGame b = new BowlingGame("[8,2][5,5][7,2][3,6][4,4][5,3][3,3][4,5][8,1][2,6]");
+		assertEquals(98, b.getScore());
+	}
 
+	@Test
+	public void strikes2() {
+		BowlingGame b = new BowlingGame("[10,0][10,0][7,2][3,6][4,4][5,3][3,3][4,5][8,1][2,6]");
+		assertEquals(112, b.getScore());
+	}
+	
+	@Test
+	public void strikeSpare() {
+		BowlingGame b = new BowlingGame("[10,0][4,6][7,2][3,6][4,4][5,3][3,3][4,5][8,1][2,6]");
+		assertEquals(103, b.getScore());
+	}
+	
+	@Test
+	public void spareLast() {
+		BowlingGame b = new BowlingGame("[1,5][3,6][7,2][3,6][4,4][5,3][3,3][4,5][8,1][2,8][7]");
+		assertEquals(90, b.getScore());
+	}
+	
+	@Test
+	public void strikeLast() {
+		BowlingGame b = new BowlingGame("[1,5][3,6][7,2][3,6][4,4][5,3][3,3][4,5][8,1][10,0][7,2]");
+		assertEquals(92, b.getScore());
+	}
+
+	@Test
+	public void spareStrike2() {
+		BowlingGame b = new BowlingGame("[1,5][3,6][7,2][3,6][4,4][5,3][3,3][4,5][8,1][2,8][10]");
+		assertEquals(93, b.getScore());
+	}
+	
+	@Test
+	public void perfect() {
+		BowlingGame b = new BowlingGame("[10,0][10,0][10,0][10,0][10,0][10,0][10,0][10,0][10,0][10,0][10,10]");
+		assertEquals(300, b.getScore());
+	}
 }
